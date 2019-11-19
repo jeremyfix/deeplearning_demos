4e30f2c22561d25b1dbf777d6d4df9bd72a8601b
Jeremy Fix <jeremy.fix@loria.fr>
2019-11-19 15:28:45 +0100

    adding the entry point for the client

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     license='LICENSE.txt',
     description='Some scripts to easily raise up deep learning demos',
     entry_points={
-        'console_scripts': ['segmentation_server=deeplearning_demos.segmentation_server:main']
+        'console_scripts': ['segmentation_server=deeplearning_demos.segmentation_server:main',
+                            'segmentation_client=deeplearning_demos.client:main']
     }
 )
