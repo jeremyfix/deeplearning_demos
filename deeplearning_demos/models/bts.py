7fdde0cdfe18e8c455aa17b34e3e14894c8b1302
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2020-06-29 11:56:50 +0200

    bts basis

---
+++ deeplearning_demos/models/bts.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+"""
+This scripts provides an interface to the bts library
+"""
+
+try:
+    import bts
+except ImportError:
+    print("Warning: cannot import bts")
+
+
+class BTS:
+
+    def __init__(self, cfg):
+        pass
+
+    def __call__(self, ndimage):
+        '''
+        Process an image through the model
+        and returns a Gray image
+        '''
+        return ndimage
