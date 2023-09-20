e586b57475019ffc1dfcf3b26f2d37a0bc397874
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-09-20 16:52:53 +0200

    simple first cli client

---
+++ dlclient/dlclient/cli_client.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# This file is part of dlclient.
+
+# dlclient is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+# dlclient is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with dlclient. If not, see <https://www.gnu.org/licenses/>.
+
+# Standard imports
+import logging
+import sys
+import argparse
+import socket
+
+
+def main():
+
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format="%(message)s")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--hostname",
+        default="localhost",
+        type=str,
+        help="The host to connect to",
+        action="store",
+    )
+    parser.add_argument(
+        "--port",
+        default=6008,
+        type=int,
+        help="The port on which to connect",
+        action="store",
+    )
+    args = parser.parse_args()
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((args.hostname, args.port))
+        sock.sendall(bytes("list", "ascii"))
+        response = str(sock.recv(1024), "ascii")
+        print(response)
+
+
+if __name__ == "__main__":
+    main()
