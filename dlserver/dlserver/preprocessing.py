90f6a6bfc706da983a8e8bcd1dd8f7f6c8eb16a2
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-10-11 11:52:31 +0200

    Segmentation-VOC-FCN-Resnet50-12-int8 is working, thanks to the new prpeprocessing pipeline

--- dlserver/dlserver/preprocessing.py
+++ dlserver/dlserver/preprocessing.py
@@ -14,6 +14,9 @@
 # You should have received a copy of the GNU General Public License along with
 # dlserver. If not, see <https://www.gnu.org/licenses/>.
 
+# Standard imports
+import logging
+
 # External imports
 import numpy as np
 from PIL import Image
@@ -110,73 +113,6 @@ def imagenet_preprocess():
     return load_compose(preprocessing_params)
 
 
-def OLD_imagenet_preprocess(img: np.array, frame_assets: dict):
-    """
-    img: np.array
-        The input image in (H, W, C)
-
-    Returns: np.array
-        The input tensor in (1, C, H, W)
-    """
-    img = img / 255.0  # in [0, 1], as float32
-
-    # Apply the ImageNet normalization
-    imagenet_mean = np.array([0.485, 0.456, 0.406])
-    imagenet_std = np.array([0.229, 0.224, 0.225])
-    img = (img - imagenet_mean) / imagenet_std
-
-    # Pad the image to a square image
-    largest_size = max(img.shape[0], img.shape[1])
-    pad_w = largest_size - img.shape[0]
-    pad_h = largest_size - img.shape[1]
-    img = np.pad(
-        img,
-        pad_width=(
-            (pad_w // 2, pad_w - pad_w // 2),
-            (pad_h // 2, pad_h - pad_h // 2),
-            (0, 0),
-        ),
-    )
-
-    if largest_size > 224:
-        # If the image is larger than 224, we center crop
-
-        # Crop centered window 224x224
-        # Borrowed from https://github.com/onnx/models
-        # which is Licensed Apache 2
-        def crop_center(image, crop_w, crop_h):
-            h, w, c = image.shape
-            start_y = h // 2 - crop_h // 2
-            start_x = w // 2 - crop_w // 2
-            return image[start_y : start_y + crop_h, start_x : start_x + crop_w, :]
-
-        img = crop_center(img, 224, 224)
-    else:
-        # Otherwise we pad again
-        # Pad, if necessary, the image to be (224, 224)
-        pad_h = 224 - img.shape[0]
-        pad_w = 224 - img.shape[1]
-        img = np.pad(
-            img,
-            pad_width=(
-                (pad_h // 2, pad_h - pad_h // 2),
-                (pad_w // 2, pad_w - pad_w // 2),
-                (0, 0),
-            ),
-        )
-
-    frame_assets["resized_img"] = (
-        (img.copy() * imagenet_std + imagenet_mean) * 255
-    ).astype(np.uint8)
-
-    # Finally convert the image from (H, W, C) to (1, C, H, W)
-    img = img.transpose(2, 0, 1)
-    img = img[np.newaxis, ...]
-    img = img.astype("float32")
-
-    return img
-
-
 def compose(inarray, fns: list, frame_assets: dict):
     for fn in fns:
         inarray = fn(inarray, frame_assets)
@@ -189,16 +125,21 @@ def load_compose(preprocessings: list):
     fns = []
     for fn_params in preprocessings:
         fn, params = next(iter(fn_params.items()))
-        fns.append(
-            eval(
-                f"lambda inarray, frame_assets, params=params:{fn}(inarray, frame_assets=frame_assets,**params)"
+        if params is None:
+            fn_i = eval(
+                f"lambda inarray, frame_assets, params=params:{fn}(inarray,frame_assets=frame_assets)"
             )
-        )
+        else:
+            fn_i = eval(
+                f"lambda inarray, frame_assets, params=params:{fn}(inarray,frame_assets=frame_assets,**params)"
+            )
+        fns.append(fn_i)
     return lambda inarray, frame_assets: compose(inarray, fns, frame_assets)
 
 
 def load_function(preprocessings: str | list):
     # preprocessing can be either a string or a dictionary
+    logging.debug(f"Loading the preprocessing from {preprocessings}")
     if isinstance(preprocessings, str):
         return eval(preprocessings)()
     else:
